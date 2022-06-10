1ce9a627f11066dd2dce7b344290d514641f54bc
vamsi7659 <107235584+vamsi7659@users.noreply.github.com>
2022-06-10 10:14:18 +0530

    Add files via upload

---
+++ firstcode.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	var urname string
+	fmt.Println("what is your name")
+	fmt.Scan(&urname)
+	if urname == "vamsi" {
+		vamsi := user{"vamsi", "vamsi@gmail.com", 20, true}
+		myage := 30
+		mymail := "vamsi@gmail.com"
+		hisage := &myage
+		mail := &mymail
+
+		names := []string{"vamsi"}
+		nameage := make(map[string]int)
+		nameage["vamsi"] = 20
+		names = append(names, "vamsi", "vinay", "vivek")
+		fmt.Println(names)
+		fmt.Println(nameage)
+		fmt.Println(vamsi)
+		fmt.Println(*hisage)
+		fmt.Println(*mail)
+
+	} else {
+		fmt.Println("not allowed kid")
+
+	}
+
+}
+
+type user struct {
+	name   string
+	mail   string
+	age    int
+	active bool
+}
